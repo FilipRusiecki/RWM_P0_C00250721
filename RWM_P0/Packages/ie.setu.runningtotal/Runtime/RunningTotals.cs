55c6205a0d9929c74b34183ede2c65c6dd234ebe
FilipRusiecki <57904846+FilipRusiecki@users.noreply.github.com>
2022-09-12 14:03:44 +0100

    Update RunningTotals.cs

--- RWM_P0/Packages/ie.setu.runningtotal/Runtime/RunningTotals.cs
+++ RWM_P0/Packages/ie.setu.runningtotal/Runtime/RunningTotals.cs
@@ -2,14 +2,15 @@ public class RunningTotal
 {
     public static int[] runningTotals(int[] xs)
     {
-        //int currentTotal = 0;
+        int currentTotal = 0;
 
         int[] result = new int[xs.Length];
         //result[0] = 0;
         for (int i = 0; i < xs.Length; i++)
         {
-            result[0] = 0;
-            result[i] += xs[i];
+
+            currentTotal += xs[i];
+            result[i] = currentTotal;
         }
         return result;
     }
